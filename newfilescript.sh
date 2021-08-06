bd8235ee48aaa3f1a54e7813418dcc1a51a37e54
ravikumar <mail2ravikumaryenda@gmail.com>
2021-08-06 12:39:10 +0530

    Updated Script to delete the unused files

--- newfilescript.sh
+++ newfilescript.sh
@@ -1,19 +1,33 @@
 #!/bin/bash
 
-ls -lrt /etc/ > ./newone
+working_dir=$(pwd)
+
+if [ -f ${working_dir}/dir ]; then
+	rm -rf ${working_dir}/dir
+fi
+
+
+if [ -f ${working_dir}/files ]; then
+	rm -rf ${working_dir}/files
+fi
+
+> ${working_dir}/diroutput
+> ${working_dir}/fileoutput
+
+ls -lrt /etc/ > ${working_dir}/newone
 
 while read -r line 
 do 
 #	echo "${line}" | grep ^d | awk '{ print $NF }' >> ./DirectoryNames
 #	echo "${line}" | grep ^- | awk '{ print $NF }' >> ./FileNames]
-      echo "${line}" | grep ^d >> ./dir
-      echo "${line}" | grep ^- >> ./files
+      echo "${line}" | grep ^d >> ${working_dir}/dir
+      echo "${line}" | grep ^- >> ${working_dir}/files
 done < "./newone"
 while read -r dir
 do 	
-  	echo "${dir}" | rev | cut -d " " -f1 | rev >> ./diroutput
+  	echo "${dir}" | rev | cut -d " " -f1 | rev >> ${working_dir}/diroutput
 done < "./dir" 
 while read -r files
 do 
- 	echo "${files}" | rev | cut -d " " -f1 | rev >> ./fileoutput 
+ 	echo "${files}" | rev | cut -d " " -f1 | rev >> ${working_dir}/fileoutput 
 done < "./files"
